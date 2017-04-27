abe3d73524b2d69bb0f5ac034f1217028ee5663f
p-archer <peter.archer86@gmail.com>
2017-04-27 21:52:57 +0100

    - added getFitness, finished init function, added some visuals

--- readme.md
+++ readme.md
@@ -2,24 +2,25 @@
 <h2>Table of Contents</h2>
 <div id="text-table-of-contents">
 <ul>
-<li><a href="#org5560860">1. Description</a></li>
-<li><a href="#orgad74a93">2. Algorithm overview</a></li>
+<li><a href="#org1c08396">1. Description</a></li>
+<li><a href="#orgbd7e5ac">2. Algorithm overview</a></li>
 </ul>
 </div>
 </div>
 
 
-<a id="org5560860"></a>
+<a id="org1c08396"></a>
 
 # Description
 
 Small js application using genetic algorithm to place 8 queens on a chess board. All the examples I found on the internet where unnecessarily complicated and long. This aims to be dumb and short, but effective.
 
 
-<a id="orgad74a93"></a>
+<a id="orgbd7e5ac"></a>
 
 # Algorithm overview
 
 The algorithm is kept very simple.
+
 ![img](./docs/alg.png)
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,114 @@
-$(document).ready(() => {
-	console.log('started');
-});
+const generationSize = 10;
+const boardSize = 8*8;
+const geneCount = 8;
+const mutationFactor = 1; // 1/1000
+
+let generation = [];
 
 function start() {
 	console.log('start clicked');
+	console.log(JSON.stringify(initialiseGeneration(), null, 2));
 }
 
+function initialiseGeneration() {
+	let result = [];
+	const cols = Math.sqrt(boardSize); //assuming board is square
+
+	for (let i=0; i<generationSize; i++) {
+		let current = {
+			genes: [],
+			fitness: 0
+		};
+
+		for (let j=0; j<geneCount; j++) {
+			current.genes.push(j * cols + j); //place queens on the main diagonal
+		}
+
+		for (let j=0; j<geneCount; j++) { //randomise queens' positions
+			let aIndex = Math.floor(Math.random() * cols);
+			let bIndex = Math.floor(Math.random() * cols); //can be the same queen, intentional
+
+			let a = { x: current.genes[aIndex] % cols, y: Math.floor(current.genes[aIndex] / cols) };
+			let b = { x: current.genes[bIndex] % cols, y: Math.floor(current.genes[bIndex] / cols) };
+
+			current.genes[aIndex] = a.x + b.y * cols;
+			current.genes[bIndex] = a.y * cols + b.x;
+		}
+
+		current.genes.sort((a, b) => {
+			return a - b;
+		});
+
+		current.fitness = getFitness(current.genes);
+		result.push(current);
+	}
+
+	result.sort((a, b) => {
+		return b.fitness - a.fitness;
+	});
+
+	show(result[0].genes);
+	return result;
+}
+
+function findParents(generation) {
+}
+
+function createChildren(parents) {
+}
+
+function mutate(genes) {
+}
+
+function getFitness(genes) {
+	const cols = Math.sqrt(boardSize);
+	let fitness = 0;
+
+	let columns = []; //counter for columns occupied by queens
+	let rows = []; //same for rows
+
+	genes.forEach((g) => {
+		columns[g % cols] = 1;
+		rows[Math.floor(g / cols)] = 1;
+	});
+
+	fitness += Object.keys(columns).length;
+	fitness += Object.keys(rows).length;
+
+	//diagonals
+	let diagonal = [];
+	let counterdiagonal = [];
+	for (let i=0; i<genes.length; i++) {
+		let x = genes[i] % cols;
+		diagonal = genes.filter((g) => {
+			return Math.abs(g - x) % (cols + 1) === 0;
+		});
+		counterdiagonal = genes.filter((g) => {
+			return Math.abs(g - x) % (cols - 1) === 0;
+		});
+
+		if (diagonal.length === 1) {
+			fitness++;
+		}
+		if (counterdiagonal === 1) {
+			fitness++;
+		}
+	}
+
+	return fitness;
+}
+
+function show(genes) {
+	const cols = Math.sqrt(boardSize);
+
+	//clear board
+	$('.table-container .table .row .tile').removeClass('queen');
+
+	genes.forEach((g) => {
+		let column = g % cols;
+		let row = Math.floor(g / cols);
+
+		let selector = '.table-container .table .row:nth-child(' + (row+1) + ') .tile:nth-child(' + (column+1) + ')';
+		$(selector).addClass('queen');
+	});
+}
